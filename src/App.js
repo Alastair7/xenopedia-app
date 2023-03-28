c46d5b71df4c0a17019cfc6e9f1b3967148abaac
Alastair7 <alexandru.ban.drmer@gmail.com>
2023-03-29 00:27:12 +0200

    Add data table

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import React from 'react'
 import 'primereact/resources/themes/viva-dark/theme.css'
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import AppTabMenu from './components/ui/Menu/menu';
 import Logo from './components/ui/Logo/logo.js';
+import AppDataTable from './components/ui/DataTable/DataTable';
 
 function App() {
   return (
     <div className='App'>
       <Logo />
       <AppTabMenu />
+      <AppDataTable />
+      
 
     </div>    
   );
---
+++ src/components/ui/DataTable/DataTable.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Moment from 'moment'
+import { DataTable } from 'primereact/datatable';
+import { Column } from  'primereact/column';
+import './DataTable.scss'
+
+export default function AppDataTable() {
+
+    const date = Moment();
+    const APIData = [{'idText':1, 'title':'Test', 'createdBy': 'Fyrek', 'creationDate': date.format('DD/MM/yyyy')}];
+    return (
+        <DataTable value= {APIData} tableStyle= {{ minwidth: '50rem'}}>
+            <Column field='idText' header='Id'></Column>
+            <Column field='title' header='Title'></Column>
+            <Column field='createdBy' header='CreatedBy'></Column>
+            <Column field='creationDate' header='CreationDate'></Column>
+        </DataTable>
+    );
+}
