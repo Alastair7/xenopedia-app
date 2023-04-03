ddd43ec9cbc29769ce7266d3c0df1d04600c4a8f
Alastair7 <alexandru.ban.drmer@gmail.com>
2023-04-03 22:49:13 +0200

    Add axios and modify API request

--- src/data/api/text/get-text.js
+++ src/data/api/text/get-text.js
@@ -1,6 +1,23 @@
-var response = fetch('https://localhost:32768/Text/GetTextAll')
-.then(response => {
-    return response.json();
-});
+import { useEffect, useState } from "react";
+import { Axios } from "axios";
 
-export { response }
+const Texts = () => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+
+        Axios.get('https://localhost:32768/Text/GetTextAll')
+        .then((response) => {
+            console.log(response.data);
+            setData(response.data);
+        });
+            
+    }, [data]);
+
+    if (!data) return null;
+
+    return Texts;
+}
+
+export default Texts();
+
+    
