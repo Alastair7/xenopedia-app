10d099e97b947e3d1c24af3089528e3ef7912766
Alastair7 <alexandru.ban.drmer@gmail.com>
2023-04-03 22:49:29 +0200

    Add Axios

--- src/components/ui/DataTable/DataTable.js
+++ src/components/ui/DataTable/DataTable.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import Moment from 'moment';
 import { DataTable } from 'primereact/datatable';
 import { Column } from  'primereact/column';
 import './DataTable.scss';
-import { response } from '../../../data/api/text/get-text.js'
+import Texts from '../../../data/api/text/get-text';
 
 export default function AppDataTable() {
 
-    const date = Moment();
-    var data = response;
-
-    const APIData = [{'idText':1, 'title':'Test', 'createdBy': 'Fyrek', 'creationDate': date.format('DD/MM/yyyy')}];
     return (
-        <DataTable value= {data} tableStyle= {{ minwidth: '50rem'}}>
-            <Column field='idText' header='Id'></Column>
-            <Column field='title' header='Title'></Column>
-            <Column field='createdBy' header='CreatedBy'></Column>
-            <Column field='creationDate' header='CreationDate'></Column>
+        <DataTable value={Texts}>
+            {Texts.map((col, i) => (
+                <Column key={col.field} field={col.field} header={col.header} />
+            ))}
         </DataTable>
     );
 }
