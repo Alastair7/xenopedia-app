a2ae245383cf8f04bcbf72d7dbaff5803ed69b8e
Alastair7 <alexandru.ban.drmer@gmail.com>
2023-04-08 23:29:25 +0200

    Fix Add Text component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react'
-import 'primereact/resources/themes/viva-dark/theme.css'
+import React, { useState } from 'react';
+import 'primereact/resources/themes/viva-dark/theme.css';
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import Logo from './components/ui/Logo/logo.js';
 import AppDataTable from './components/ui/DataTable/DataTable';
-import AddButton from './components/ui/AddButton/Button';
-import NewTextForm from './components/form/NewText/NewText..js';
+import NewTextForm from './components/form/NewText/NewText.js';
+import AddButton from './components/ui/AddButton/Button.js';
 
 function App() {
   const [showForm, setShowForm] = useState(false);
-  const formVisibility = () => {
-      setShowForm(!showForm);
+
+  const openForm = () => {
+    setShowForm(true);
   }
+
+  const closeForm = () => {
+    setShowForm(false);
+  }
+  
   return (
     <div className='App'>
       <Logo />
-      <AddButton />
+      <AddButton onClick={ openForm } />
+      {showForm && <NewTextForm onClose={ closeForm } />}
       <AppDataTable />
-      <NewTextForm />
-      
-
     </div>    
   );
 }
--- src/components/ui/AddButton/Button.js
+++ src/components/ui/AddButton/Button.js
@@ -1,8 +1,8 @@
-import React from 'react'
 import { Button } from 'primereact/button'
 
-export default function AddButton() {
+export default function AddButton(props) {
+
     return (
-        <Button label='Add' icon='pi pi-plus' iconPos='left'/>
+        <Button label='Add' icon='pi pi-plus' iconPos='left' onClick={props.onClick}/>
     );
 };
