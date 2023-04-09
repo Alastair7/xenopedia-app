d8738c78a1aa85860064c3ba72b3b4738a4c0693
Alastair7 <alexandru.ban.drmer@gmail.com>
2023-04-09 12:38:17 +0200

    Add filter and pagination to datatable

--- src/components/ui/DataTable/DataTable.js
+++ src/components/ui/DataTable/DataTable.js
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { DataTable } from 'primereact/datatable';
+import { Paginator } from 'primereact/paginator';
 import { Column } from  'primereact/column';
 import './DataTable.scss';
 import axios from 'axios';
 
 export default function AppDataTable() {
     const [texts, setTexts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
+
+    const pageSize = 2;
+
+    const onPageChange = (event) => {
+        setCurrentPage(event.page);
+    }
         
     useEffect(() => {
         axios.get("https://localhost:32768/Text/GetTextAll")
@@ -13,13 +21,26 @@ export default function AppDataTable() {
             setTexts(response.data)
         });
     });
+
+    console.log(texts)
         
     return (
-        <DataTable value={texts}>
-        <Column field="idText" header = 'ID' />
-        <Column field="title" header = 'Title' />
-        <Column field="createdBy" header = 'Created By' />
-        <Column field="creationDate" header= 'Creation Date' />
-    </DataTable>
+        <>
+            <DataTable 
+            value={texts.slice(currentPage * pageSize, (currentPage + 1) * pageSize)} globalFilter
+            rows={pageSize} 
+            totalRecords={texts.length} 
+            onPageChange={onPageChange}>
+                <Column field="idText" header = 'ID' />
+                <Column field="title" header = 'Title' />
+                <Column field="createdBy" header = 'Created By' />
+                <Column field="creationDate" header= 'Creation Date' />
+            </DataTable>
+
+            <Paginator first={currentPage * pageSize}
+            rows={pageSize}
+            totalRecords={texts.length}
+            onPageChange={onPageChange} />
+        </>
     );
 }
