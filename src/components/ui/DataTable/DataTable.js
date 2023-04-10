4618d8e298766c7d6d4364f6b65f9956d8ab1e62
Alastair7 <alexandru.ban.drmer@gmail.com>
2023-04-10 13:18:29 +0200

    Fix Paginator and Datatable

--- src/components/ui/DataTable/DataTable.js
+++ src/components/ui/DataTable/DataTable.js
@@ -1,46 +1,59 @@
 import React, { useEffect, useState } from 'react';
 import { DataTable } from 'primereact/datatable';
-import { Paginator } from 'primereact/paginator';
 import { Column } from  'primereact/column';
 import './DataTable.scss';
 import axios from 'axios';
 
 export default function AppDataTable() {
     const [texts, setTexts] = useState([]);
+    const [loading, setLoading] = useState(true);
     const [currentPage, setCurrentPage] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
 
-    const pageSize = 2;
+    const pageSize = 10;
 
     const onPageChange = (event) => {
-        setCurrentPage(event.page);
+        setCurrentPage(event.first / event.rows);
     }
-        
+
+    const firstIndex = currentPage * pageSize;
+    const lastIndex = firstIndex + pageSize;
+    const currentData = texts.slice(firstIndex, lastIndex);
+
     useEffect(() => {
+        getTextData();
+    }, []);
+
+    const getTextData = () => {
+
         axios.get("https://localhost:32768/Text/GetTextAll")
         .then((response) => {
-            setTexts(response.data)
-        });
-    });
+            const totalRecords = response.data.length;
 
-    console.log(texts)
-        
+            setTotalPages(Math.ceil(totalRecords / pageSize))
+            setTexts(response.data)
+            setLoading(false);
+
+        }).catch(error => {
+            console.log(error);
+            setLoading(false);
+        });
+    }
+
     return (
         <>
             <DataTable 
-            value={texts.slice(currentPage * pageSize, (currentPage + 1) * pageSize)} globalFilter
-            rows={pageSize} 
-            totalRecords={texts.length} 
-            onPageChange={onPageChange}>
+            value={currentData}
+            rows={pageSize}
+            loading={loading}
+            paginator
+            onPage={onPageChange}
+            totalPages={totalPages}>
                 <Column field="idText" header = 'ID' />
                 <Column field="title" header = 'Title' />
                 <Column field="createdBy" header = 'Created By' />
                 <Column field="creationDate" header= 'Creation Date' />
             </DataTable>
-
-            <Paginator first={currentPage * pageSize}
-            rows={pageSize}
-            totalRecords={texts.length}
-            onPageChange={onPageChange} />
         </>
     );
 }
